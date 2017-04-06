5d04808d7e7f4c362bc721b5ed5be96d28d8243f
avrahamluna <avraham.luna@gmail.com>
2017-04-05 19:35:27 -0700

    initial commit & setting up User Schema

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var morgan = require('morgan');
+
+
+var app = express();
+
+//Middleware
+app.use(morgan('dev'));
+
+
+app.get('/', function(req, res){
+    var name = "Abram";
+    res.json("My name is " + name);
+});
+
+app.get('/catname', function(req, res){
+   res.json('batman'); 
+});
+
+
+app.listen(3000, function (err){
+    if(err) throw err;
+    console.log("Server is Running");
+    
+});
