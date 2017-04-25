f94241738d9e3a55851efcb205d113739b9bf034
avrahamluna <avraham.luna@gmail.com>
2017-04-24 18:35:46 -0700

    Configure Database with MongoLab

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
-
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect( 'mongodb://root:eluniversal@ds119151.mlab.com:19151/ecommerce', function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Connected to the database");
+    }
+});
 
 //Middleware
 app.use(morgan('dev'));
